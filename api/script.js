404714e0941ea3ce8ada1e714b804a8f500bece6
Lucam! <136206047+LUC4Mdv@users.noreply.github.com>
2024-11-03 21:03:13 -0300

    up

--- api/script.js
+++ api/script.js
@@ -1,16 +1,17 @@
 import express from "express"
-import axios from "axios"
-import { MercadoPagoConfig, Payment } from "mercadopago"
-import process from "process"
+import { MercadoPagoConfig, Payment, Preference } from "mercadopago"
+
+
+const client = new MercadoPagoConfig({  accessToken: 'APP_USR-8133444984667352-110316-55fc2ba574dd770c83ae0d634a74462f-2076107374' });
+const preference = new Preference(client);
+
 
 const app = express()
 
 app.use(express.json())
 
-export function gerarLinkPagamento(){
-    let sdk = MercadoPagoConfig.SDK(process.env.KEYMP)
-
-    payment_data = {
+    
+    const body = {
         items: [
             {
               id: '1',
@@ -20,86 +21,24 @@ export function gerarLinkPagamento(){
               category_id: 'vips',
               quantity: 1,
               currency_id: 'BRL',
-              unit_price: 2,
+              unit_price: 2.50,
         }],
-            transaction_amountt: Number(req.headers.value),
-            payment_method_id: "pix",
-            payer: { 
-            name: "Player"
-        }
+        back_urls: {
+            success: 'https://hardtuber.site/sucesso',
+            failure: 'https://test.com/failure',
+            pending: 'https://test.com/pending',
+          },
     }
-    let result = sdk.preference().create(payment_data)
-    let payment = result["response"]
-    let linkIniciarPagamento = payment['init_point']
-    return linkIniciarPagamento
-}
+    
 
 
-app.get("/api/pagamento_atualizado", async (req, res) => {
-    const information = await new Payment({
-        accessToken: process.env.KEYMP
-    })
-    .get({
-        id: req.header.paymentid
-    })
-    .cacth(console.log)
+    const response = await preference.create({ body })
+    .then(console.log).catch(console.log);
 
-    res.send({response: information}).status(200)
-})
 
-app.put("api/criar_pagamento", async (req,res) => {
-    await new Payment ({
-        accessToken: process.env.KEYMP
-    })
-    .create({
-        body: {
-            items: [
-                {
-                  id: '1',
-                  title: 'Vip Gold',
-                  description: '» Veja todas as informações e benefícios deste VIP, em nosso servidor do Discord!',
-                  picture_url: 'https://www.hardtuber.site/src/img/vips/Gold.png',
-                  category_id: 'vips',
-                  quantity: 1,
-                  currency_id: 'BRL',
-                  unit_price: 2,
-            }],
-                transaction_amountt: Number(req.headers.value),
-                payment_method_id: "pix",
-                payer: { 
-                name: "Player"
-            }
-        }
-    })
-    .then((response) => {
-        res.send({
-            response: response.point_of_interaction_off_interaction.transaction_data.ticket_url
-        })
-    })
-    .cacth(console.log)
-})
 
-app.post("/api/pagamento_atualizado", async (req, res) => {
-    if(req.body.action === "payment.updated") {
-        await axios({
-            url: "/api/pagamento_atualizado",
-            headers: {
-                paymentid: req.body.data.id
-            }
-        })
-        .then(x => x.data)
-        .then(async (r) => {
-            if(
-            r.response.satus === 'approved'
 
-            ){
-                console.log(r)
-            }
-        })
-        .catch(console.log)
-    }
-})
 
-app.listen(process.env.PORT, async () => {
-    console.log(`api online na porta ${process.env.PORT}`)
-})
+
+
+
